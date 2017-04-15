8b66179843345257a005efceab9d64389a4d7698
atul-askonline <atul@askonlinesolutions.com>
2017-04-15 16:47:24 +0530

    Changes

--- app/src/main/java/com/netreadystaging/godine/fragments/ContactMapFragment.java
+++ app/src/main/java/com/netreadystaging/godine/fragments/ContactMapFragment.java
@@ -1,12 +1,18 @@
 package com.netreadystaging.godine.fragments;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -30,7 +36,8 @@ public class ContactMapFragment extends Fragment implements OnMapReadyCallback {
 
     View view  ;
     private List<Restaurant> listRestaurants;
-
+GoogleMap mymap;
+    SupportMapFragment mapFragment;
 
     @Nullable
     @Override
@@ -42,9 +49,10 @@ public class ContactMapFragment extends Fragment implements OnMapReadyCallback {
         {
             ex.printStackTrace();
         }
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+      mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mymap=mapFragment.getMap();
         Bundle bundle = getArguments();
         if(bundle!=null)
         {
@@ -61,32 +69,79 @@ public class ContactMapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         for (final Restaurant restaurant:listRestaurants) {
-            LatLng latLng = new LatLng(restaurant.lat, restaurant.lng);
+
+            final LatLng latLng = new LatLng(restaurant.lat, restaurant.lng);
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
             googleMap.addMarker(new MarkerOptions()
-                    .position(latLng)
+                    .position(latLng).snippet(restaurant.getAddress() )
                     .title(restaurant.getName()))
-
                     .setIcon(BitmapDescriptorFactory.fromResource(R.drawable.marker));
-            googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+        /*    Log.d("Restauar", "" + restaurant.getId());
+
+
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+
                 @Override
                 public boolean onMarkerClick(Marker marker) {
+                    Restaurant restaurant=mymap.get(marker);
 
-                    return false;
+                    Log.d("Restaua",""+restaurant.getId());
+                    AlertDialog.Builder builder=new AlertDialog.Builder(getContext());
+                    LayoutInflater inflater=getActivity().getLayoutInflater();
+                    View view1=inflater.inflate(R.layout.mapdetail,null);
+                    TextView Rname= (TextView) view1.findViewById(R.id.Rname);
+                    TextView tchangepass= (TextView) view1.findViewById(R.id.Rregion);
+                    tchangepass.setText(restaurant.getAddress());
+                    Rname.setText(marker.getTitle());
+                   String title= String.valueOf(new MarkerOptions().title(restaurant.getName()));
+                    Log.d("name",""+restaurant.getName());
+               //     Log.d("Region2",title);
+                   // view1.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    builder.setView(view1);
+                    builder.create();
+                    builder.show();
+                    return true;
                 }
 
-            });
-            googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
-                @Override
-                public void onInfoWindowClick(Marker marker) {
+            });*/
+         /*  googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+               @Override
+               public boolean onMarkerClick(Marker marker) {
+                   if(marker.getSnippet() ==null) {
+                       googleMap.moveCamera(CameraUpdateFactory.zoomIn());
+                       return true;
+                   }
+                   AlertDialog.Builder builder=new AlertDialog.Builder(getContext());
+                   LayoutInflater inflater=getActivity().getLayoutInflater();
+                   View view1=inflater.inflate(R.layout.mapdetail,null);
+                  *//* TextView Rname= (TextView) view1.findViewById(R.id.Rname);
+                   Rname.setText(restaurant.getName());*//*
+                        return true;
+               }
+           });*/
 
-                }
-            });
-        }
+        /*googleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
     }
 
+    @Override
+    public View getInfoContents(Marker marker) {
+googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        View view1=getActivity().getLayoutInflater().inflate(R.layout.mapdetail,null);
+        TextView Rname= (TextView) view1.findViewById(R.id.Rname);
+        TextView tchangepass= (TextView) view1.findViewById(R.id.Rregion);
+        Rname.setText(restaurant.getName());
+        tchangepass.setText(restaurant.getAddress());
+        return view1;
+    }
+        }   );*/
+        }
+
+    }
 
     public void onDestroyView()
     {
@@ -99,4 +154,33 @@ public class ContactMapFragment extends Fragment implements OnMapReadyCallback {
             ft.commit();
         }
     }
+    class Yourcustominfowindowadpater implements GoogleMap.InfoWindowAdapter
+    {
+        View mymarkerview;
+        Yourcustominfowindowadpater()
+        {
+            mymarkerview = getActivity().getLayoutInflater()
+                    .inflate(R.layout.mapdetail, null);
+        }
+        @Override
+        public View getInfoWindow(Marker marker) {
+            render(marker, mymarkerview);
+
+            return mymarkerview;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            return null;
+        }
+        private void render(Marker marker, View view) {
+            // Add the code to set the required values
+            // for each element in your custominfowindow layout file
+            TextView Rname= (TextView) view.findViewById(R.id.Rname);
+            TextView tchangepass= (TextView) view.findViewById(R.id.Rregion);
+
+        }
+
+    }
 }
+
